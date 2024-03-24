5be8ab1555405accbaa640cece3a0303c20fd185
Wiliam Roza <devfrontwill@gmail.com>
2024-03-23 22:36:05 -0300

    upgrade

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    fontFamily:{
+      'sans': ['Roboto', 'sans-serif']
+    },
     extend: {
       backgroundImage:{
         "home": "url('/assets/bg.png')"
